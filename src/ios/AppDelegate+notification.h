fc18ea62361fcf211b922013dda92d73362ff5ad
fidoboy <fidoboy@users.noreply.github.com>
2014-12-15 11:00:43 +0100

    Update AppDelegate+notification.h

--- src/ios/AppDelegate+notification.h
+++ src/ios/AppDelegate+notification.h
@@ -11,7 +11,7 @@
 @interface AppDelegate (notification)
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo;
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler;
 - (void)applicationDidBecomeActive:(UIApplication *)application;
 
 @property (nonatomic, retain) NSDictionary	*launchNotification;
